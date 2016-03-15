74d4be37937c64ee38dcb8075ac27a6eaf4e47b6
sergiy.bezzub <sergiy.bezzub@HRK1-LHP-F03172.synapse.com>
2016-03-15 20:07:22 +0200

    add new images

--- src/main/java/com/autodetect/parking/ImagePath.java
+++ src/main/java/com/autodetect/parking/ImagePath.java
@@ -1,23 +1,33 @@
 package com.autodetect.parking;
 
 public enum ImagePath {
-	EMPTY3("/empty3.png"),
-	BUSY1("/busy1.png"),
-	BUSY2("/busy2.png"),
-	BUSY3("/busy3.png"),
-	BUSY4("/busy4.png"),
-	BUSY5("/busy5.png"),
-	BUSY6("/busy6.png"),
-	BUSY7("/busy7.png"),
-	BUSY8("/busy8.png"),
-	EMPTY1("/empty1.png"),
-	EMPTY2("/empty2.png"),
-	EMPTY4("/empty7.png"),
-	EMPTY5("/empty5.png"),
-	EMPTY6("/empty6.png"),
-	EMPTY7("/empty4.png"),
-	EMPTY8("/empty8.png");
+	EMPTY1("/e1.png"),
+	EMPTY2("/e2.png"),
+	EMPTY3("/e3.png"),
+	BUSY1("/b1.png"),
+	BUSY2("/b2.png"),
+	BUSY3("/b3.png"),
+	BUSY4("/b4.png"),
+	BUSY5("/b5.png"),
+	BUSY6("/b6.png");
 	
+//	EMPTY3("/empty3.png"),
+//	BUSY1("/busy1.png"),
+//	BUSY2("/busy2.png"),
+//	BUSY3("/busy3.png"),
+//	BUSY4("/busy4.png"),
+//	BUSY5("/busy5.png"),
+//	BUSY6("/busy6.png"),
+//	BUSY7("/busy7.png"),
+//	BUSY8("/busy8.png"),
+//	EMPTY1("/empty1.png"),
+//	EMPTY2("/empty2.png"),
+//	EMPTY4("/empty7.png"),
+//	EMPTY5("/empty5.png"),
+//	EMPTY6("/empty6.png"),
+//	EMPTY7("/empty4.png"),
+//	EMPTY8("/empty8.png");
+
 	private String path;
 
 	private ImagePath(String path) {
